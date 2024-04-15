d96aa96fd7bd4b1b703d3a41a4327189928ee6d3
unknown <fatimahaddadou79@gmail.com>
2024-04-15 21:43:21 +0200

    fmsg

--- resources/views/enseignants/create.blade.php
+++ resources/views/enseignants/create.blade.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+@extends('layouts.app')
+
+@section('title', 'enseignant')
+
+@section('content')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Enseignants</title>
-    <link rel="stylesheet" href="{{ asset('assets/app.css')}}">
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
@@ -109,8 +111,7 @@
     }
 
 </style>
-</head>
-<body>
+
 
 
 <div class="container">
@@ -184,8 +185,10 @@
     </div>
 </div>
 
+@endsection
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('assets/app.css')}}">
+@endsection
 
 
 
-</body>
-</html>
--- resources/views/enseignants/show.blade.php
+++ resources/views/enseignants/show.blade.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+@extends('layouts.app')
+
+@section('title', 'enseignant')
+
+@section('content')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Enseignants</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('assets/app.css')}}">
-</head>
-<body>
+
 
 <style>
 #ski{
@@ -113,5 +114,7 @@
 
 
 
-</body>
-</html>
+@endsection
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('assets/app.css')}}">
+@endsection
